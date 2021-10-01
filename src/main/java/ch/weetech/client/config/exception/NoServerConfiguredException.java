8976445555f79458bcb1d38bd28efd0d84702b2d
Jason Wee <peichieh@gmail.com>
2021-10-01 12:28:22 +0800

    ident

--- src/main/java/ch/weetech/client/config/exception/NoServerConfiguredException.java
+++ src/main/java/ch/weetech/client/config/exception/NoServerConfiguredException.java
@@ -2,9 +2,9 @@ package ch.weetech.client.config.exception;
 
 public class NoServerConfiguredException extends RuntimeException {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
+    /**
      * Constructs a new runtime exception with the specified detail message.
      *
      * @param   message   the detail message. The detail message is saved for
