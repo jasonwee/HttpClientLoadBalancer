ef69aa36dfb7389ccb53c79b4aecd125136af6b7
Jason Wee <peichieh@gmail.com>
2021-10-01 12:45:56 +0800

    ident

--- src/main/java/ch/weetech/params/Parameters.java
+++ src/main/java/ch/weetech/params/Parameters.java
@@ -1,10 +1,10 @@
 package ch.weetech.params;
 
 public class Parameters {
-	
+
     // 'camelCase'
     public static final String RESULT_CASING = "case";
-    
+
     //0 | 1
     public static final String REFRESH = "refresh";
 
