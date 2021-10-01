fada35c042b5e73207e5c0e6e6326a6945fc36c1
Jason Wee <peichieh@gmail.com>
2021-10-01 12:22:59 +0800

    ident

--- src/main/java/ch/weetech/client/config/idle/IdleConnectionReaper.java
+++ src/main/java/ch/weetech/client/config/idle/IdleConnectionReaper.java
@@ -8,30 +8,30 @@ import com.google.common.util.concurrent.AbstractScheduledService;
 import ch.weetech.client.config.ClientConfig;
 
 public class IdleConnectionReaper extends AbstractScheduledService {
-	
+
     final static Logger logger = LoggerFactory.getLogger(IdleConnectionReaper.class);
 
     private final ReapableConnectionManager reapableConnectionManager;
     private final ClientConfig clientConfig;
-	
+
     public IdleConnectionReaper(ClientConfig clientConfig, ReapableConnectionManager reapableConnectionManager) {
         this.reapableConnectionManager = reapableConnectionManager;
         this.clientConfig = clientConfig;
     }
 
-	@Override
-	protected void runOneIteration() throws Exception {
+    @Override
+    protected void runOneIteration() throws Exception {
         logger.debug("closing idle connections...");
         reapableConnectionManager.closeIdleConnections(clientConfig.getMaxConnectionIdleTime(),
                                                        clientConfig.getMaxConnectionIdleTimeDurationTimeUnit());
-		
-	}
 
-	@Override
-	protected Scheduler scheduler() {
+    }
+
+    @Override
+    protected Scheduler scheduler() {
         return Scheduler.newFixedDelaySchedule(0l,
                 clientConfig.getMaxConnectionIdleTime(),
                 clientConfig.getMaxConnectionIdleTimeDurationTimeUnit());
-	}
+    }
 
 }
